5c810b132d55688bf1c3d8bc8720aa5a4bdccc14
kstrodeveloper <aynerjosec@gmail.com>
2026-02-28 16:06:41 -0500

    feat: implementar UPDATE - actualizar nota

--- src/Main.java
+++ src/Main.java
@@ -213,5 +213,33 @@ public class Main {
 
         System.out.println("No encontrado");
     }
+    //UPDATE
+    public void actualizarNota(){
+        System.out.println("Codigo del estudiante:");
+        var codigo = sc.nextLine();
+
+        Estudiante e = buscarPorCodigo(codigo);
+
+        if(e == null){
+            System.out.println("No existe");
+            return;
+        }
+
+        System.out.println("Nombre de la asignatura:");
+        var nombreAsignatura = sc.nextLine();
+
+        for(Nota n : listaNotas){
+            if(n.getAsignatura().getNombre().equals(nombreAsignatura)){
+                System.out.println("Nuevo valor de la nota:");
+                n.setValor(sc.nextDouble());
+                sc.nextLine();
+                System.out.println("Nota actualizada");
+                return;
+            }
+        }
+
+        System.out.println("No encontrado");
+    }
+
 
 }
