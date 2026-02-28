a8c711876cc5dc67a55005245dc08e679441f0fc
kstrodeveloper <aynerjosec@gmail.com>
2026-02-27 20:46:37 -0500

    feat: agregar encapsulamiento a clase asignatura

--- src/Asignatura.java
+++ src/Asignatura.java
@@ -1,5 +1,25 @@
 public class Asignatura {
     private String nombre;
     private int creditos;
+
+    //Constructor
+    public Asignatura(String nombre, int creditos) {
+        this.nombre = nombre;
+        this.creditos = creditos;
+    }
     
+    //Getters y Setters
+    protected String getNombre() {
+        return this.nombre;
+    }
+    protected void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    protected int getCreditos() {
+        return this.creditos;
+    }
+    protected void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
+
 }
