4b80d849a5aeb81d96370b3feeaa0d0761978b0b
Jose David De Jesus Reales Montero <jdaviddejesusreales@unicesar.edu.co>
2026-02-27 20:59:31 -0500

    feat: agregar encapsulamiento a clase Estudiante

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -4,4 +4,56 @@ public class Estudiante {
     private string apellido;
     private int edad;
     private int semestre;
+
+    public Estudiante (){
+
+    }
+
+    public Estudiante(String codigo, string nombre, string apellido, int edad, int semestre){
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.semestre = semestre;
+    }
+
+    public string getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(string codigo) {
+        this.codigo = codigo;
+    }
+
+    public string getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(string nombre) {
+        this.nombre = nombre;
+    }
+
+    public string getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(string apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
 }
