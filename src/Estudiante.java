6a60cf137753ad84aec199c793adc4d7022e1451
Jose David De Jesus Reales Montero <jdaviddejesusreales@unicesar.edu.co>
2026-02-27 22:39:39 -0500

    fix: Correccion de String

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,7 +1,7 @@
 public class Estudiante {
-    private string codigo;
-    private string nombre;
-    private string apellido;
+    private String codigo;
+    private String nombre;
+    private String apellido;
     private int edad;
     private int semestre;
 
@@ -9,7 +9,7 @@ public class Estudiante {
 
     }
 
-    public Estudiante(string codigo, string nombre, string apellido, int edad, int semestre){
+    public Estudiante(String codigo, String nombre, String apellido, int edad, int semestre){
         this.codigo = codigo;
         this.nombre = nombre;
         this.apellido = apellido;
@@ -17,27 +17,27 @@ public class Estudiante {
         this.semestre = semestre;
     }
 
-    public string getCodigo() {
+    public String getCodigo() {
         return codigo;
     }
 
-    public void setCodigo(string codigo) {
+    public void setCodigo(String codigo) {
         this.codigo = codigo;
     }
 
-    public string getNombre() {
+    public String getNombre() {
         return nombre;
     }
 
-    public void setNombre(string nombre) {
+    public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    public string getApellido() {
+    public String getApellido() {
         return apellido;
     }
 
-    public void setApellido(string apellido) {
+    public void setApellido(String apellido) {
         this.apellido = apellido;
     }
 
