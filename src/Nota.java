1959951f0e8690960ad5c6386dcaf0e2b3046de4
kstrodeveloper <aynerjosec@gmail.com>
2026-02-27 20:59:33 -0500

    feat: agregar encapsulamiento a clase Nota

--- src/Nota.java
+++ src/Nota.java
@@ -2,5 +2,23 @@ public class Nota {
     
     private double valor;
     private Asignatura asignatura;
+    //toString
+    @Override
+    public String toString() {  
+        return "Nota{" + "valor=" + valor + ", asignatura=" + asignatura + '}';
+    }
+    //getters y setters
+    protected double getValor() {   
+        return this.valor;
+    }   
+    protected void setValor(double valor) {
+        this.valor = valor;
+    }
+    protected Asignatura getAsignatura() {
+        return this.asignatura;
+    }
+    protected void setAsignatura(Asignatura asignatura) {
+        this.asignatura = asignatura;
+    }
     
 }
